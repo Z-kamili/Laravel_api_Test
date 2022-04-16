a06c1507d609b72618d63521f881e18751afa8f5
Z-Kamili <zakariakamili97@gmail.com>
2022-04-16 17:27:12 +0000

    first commit

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    use ApiResponseTrait;
+    public function index()
+    {
+        $posts = Post::get();
+        return $this->apiResponse($posts,'ok',200);
+    }
+
+    public function show($id)
+    {
+        $post = Post::find($id);
+        if($post)
+        {
+            return $this->apiResponse($post,'ok',200);
+        }
+
+        return $this->apiResponse($post,'The post not found',200);
+       
+    }
+}
